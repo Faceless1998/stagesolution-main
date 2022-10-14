432e0c0d66e0bd4738e6887c3caa3614baa09bcc
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2022-10-14 18:11:44 +0400

    new III

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -1,12 +1,117 @@
 import React from 'react';
 import './contact.css'
 function Contact (){
-return <address>
-			You can find us here:<br />
-			GeeksforGeeks<br />
-			5th & 6th Floor, Royal Kapsons, A- 118, <br />
-			Sector- 136, Noida, Uttar Pradesh (201305)
-		</address>
+return (
+	<>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Contact Form</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12">
+					<div class="wrapper">
+						<div class="row no-gutters mb-5">
+							<div class="col-md-7">
+								<div class="contact-wrap w-100 p-md-5 p-4">
+									<h3 class="mb-4">Contact Us</h3>
+									<div id="form-message-warning" class="mb-4"></div> 
+				      		<div id="form-message-success" class="mb-4">
+				            Your message was sent, thank you!
+				      		</div>
+									<form method="POST" id="contactForm" name="contactForm" class="contactForm">
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<label class="label" for="name">Full Name</label>
+													<input type="text" class="form-control" name="name" id="name" placeholder="Name"/>
+												</div>
+											</div>
+											<div class="col-md-6"> 
+												<div class="form-group">
+													<label class="label" for="email">Email Address</label>
+													<input type="email" class="form-control" name="email" id="email" placeholder="Email"/>
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<label class="label" for="subject">Subject</label>
+													<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"/>
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<label class="label" for="#">Message</label>
+													<textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<input type="submit" value="Send Message" class="btn btn-primary"/>
+													<div class="submitting"></div>
+												</div>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+							<div class="col-md-5 d-flex align-items-stretch">
+								<div id="map">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2982.1069620128446!2d41.61024751576533!3d41.631819379242366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406785e6bb206d47%3A0xe2cd704ae9ea0ec4!2zMiDhg5Dhg6bhg5vhg5Dhg6jhg5Thg5zhg5Thg5Hhg5rhg5jhg6Eg4YOl4YOj4YOp4YOQLCDhg5Hhg5Dhg5fhg6Phg5vhg5ggNjAwMA!5e0!3m2!1ska!2sge!4v1665755831350!5m2!1ska!2sge" width="600" height="478" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			          </div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-3">
+								<div class="dbox w-100 text-center">
+			        		<div class="icon d-flex align-items-center justify-content-center">
+			        			<span class="fa fa-map-marker"></span>
+			        		</div>
+			        		<div class="text">
+				            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+				          </div>
+			          </div>
+							</div>
+							<div class="col-md-3">
+								<div class="dbox w-100 text-center">
+			        		<div class="icon d-flex align-items-center justify-content-center">
+			        			<span class="fa fa-phone"></span>
+			        		</div>
+			        		<div class="text">
+				            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+				          </div>
+			          </div>
+							</div>
+							<div class="col-md-3">
+								<div class="dbox w-100 text-center">
+			        		<div class="icon d-flex align-items-center justify-content-center">
+			        			<span class="fa fa-paper-plane"></span>
+			        		</div>
+			        		<div class="text">
+				            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+				          </div>
+			          </div>
+							</div>
+							<div class="col-md-3">
+								<div class="dbox w-100 text-center">
+			        		<div class="icon d-flex align-items-center justify-content-center">
+			        			<span class="fa fa-globe"></span>
+			        		</div>
+			        		<div class="text">
+				            <p><span>Website</span> <a href="#">yoursite.com</a></p>
+				          </div>
+			          </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	</>
+)
 }
 
 export default Contact;
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,18 +1,218 @@
 import React from 'react'
-import Hero from './Hero'
+import { NavLink } from "react-router-dom";
+
 import './home.css'
-import Insideinfo from './Insideinfo'
-import Insideplan from './Insideplan'
-import Insideservice from './Insideservice'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMugHot } from "@fortawesome/free-solid-svg-icons";
+import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import { faLayerGroup } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsSpin } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faMedal } from "@fortawesome/free-solid-svg-icons";
+
+import Photo from './../../assets/business-img.png'
+
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
+	const { t } = useTranslation();
+
   return (
 	<>
-	<Hero />
-	<Insideservice />
-	<Insideplan />
-	<Insideinfo />
-	
+	<section id="hero" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1> {t("text1")}</h1>
+          <h2> {t("text2")}</h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+	<section id="services" class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-xs-12">
+            <div class="services-item text-center">
+              <div class="icon">
+                <i class="fa-regular">
+                  <FontAwesomeIcon icon={faMedal} />
+                </i>
+              </div>
+              <h4>{t("quality")}</h4>
+              <p>
+                {t("Guaranteed quality products")}
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-xs-12">
+            <div class="services-item text-center">
+              <div class="icon">
+                <i class="fa-regular">
+                  <FontAwesomeIcon icon={faUsers} />
+                </i>
+              </div>
+              <h4> {t("customers")}</h4>
+              <p>
+               {t("Loyalty system for regular customers")}
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-xs-12">
+            <div class="services-item text-center">
+              <div class="icon">
+                <i class="fa-regular">
+                  <FontAwesomeIcon icon={faTruck} />
+                </i>
+              </div>
+              <h4> {t("delivery")}</h4>
+              <p>
+               {t("Fast and convenient delivery on time")}
+              </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+	<section id="features" class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="features-text section-header text-center">  
+              <div>   
+                <h2 class="section-title">Services We Provide</h2>
+                <div class="desc-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do <br /> eiusmod tempor incididunt ut labore et dolore.</p>
+                </div>
+              </div> 
+            </div>
+          </div>
+
+        </div>
+        <div class="row featured-bg">
+          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
+            <div class="feature-item featured-border1">
+               <div class="feature-icon float-left">
+                 <i class="fa-regular"><FontAwesomeIcon icon={faMugHot} /></i>
+               </div>
+               <div class="feature-info float-left">
+                 <h4>Easy to Customize</h4>
+                 <p>Producing long lasting organic SEO results for <br /> brand of different kinds for more than a decade,<br /> we understand that’s your.</p>
+               </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
+            <div class="feature-item featured-border2">
+               <div class="feature-icon float-left">
+                 <i class="fa-regular"><FontAwesomeIcon icon={faSuitcase} /></i>
+               </div>
+               <div class="feature-info float-left">
+                 <h4>Business Template</h4>
+                 <p>Producing long lasting organic SEO results for <br /> brand of different kinds for more than a decade,<br /> we understand that’s your.</p>
+               </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
+            <div class="feature-item featured-border1">
+               <div class="feature-icon float-left">
+                 <i class="fa-regular"><FontAwesomeIcon icon={faLightbulb}/></i>
+               </div>
+               <div class="feature-info float-left">
+                 <h4>Clean & Trendy Design</h4>
+                 <p>Producing long lasting organic SEO results for <br /> brand of different kinds for more than a decade,<br /> we understand that’s your.</p>
+               </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
+            <div class="feature-item featured-border2">
+               <div class="feature-icon float-left">
+                <i class="fa-regular"><FontAwesomeIcon icon={faLayerGroup} /></i>
+               </div>
+               <div class="feature-info float-left">
+                 <h4>Tons of Sections</h4>
+                 <p>Producing long lasting organic SEO results for <br /> brand of different kinds for more than a decade,<br /> we understand that’s your.</p>
+               </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
+            <div class="feature-item featured-border3">
+               <div class="feature-icon float-left">
+                 <i class="fa-regular"><FontAwesomeIcon icon={faArrowsSpin} /></i>
+               </div>
+               <div class="feature-info float-left">
+                 <h4>Free Future Updates</h4>
+                 <p>Producing long lasting organic SEO results for <br /> brand of different kinds for more than a decade,<br /> we understand that’s your.</p>
+               </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
+            <div class="feature-item">
+               <div class="feature-icon float-left">
+                 <i class="fa-regular"><FontAwesomeIcon icon={faCircleInfo} /></i>
+               </div>
+               <div class="feature-info float-left">
+                 <h4>Premier Support</h4>
+                 <p>Producing long lasting organic SEO results for <br /> brand of different kinds for more than a decade,<br /> we understand that’s your.</p>
+               </div>
+            </div>
+          </div>
+          
+
+        </div>
+      </div>
+    </section>
+
+	<section id="business-plan">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 pl-0 pt-70 pr-5">
+            <div class="business-item-img">
+              <img
+                src={Photo}
+                class="img-fluid"
+                alt=""
+              />
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 pl-4">
+            <div class="business-item-info">
+              <h3>Crafted For Business, Startup and Agency Websites</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do{" "}
+                <br /> tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim <br /> veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea <br /> commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate <br /> velit esse
+                cillum dolore eu fugiat nulla pariatur.
+              </p>
+
+              <NavLink class="btn btn-common" >
+                download
+                </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
 	</>
 	
   )
