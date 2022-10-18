ecb45a099f4d50b24402f455cac7458475f01104
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2022-10-18 22:26:23 +0400

    next

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,9 +1,114 @@
 import React from 'react'
 import './about.css'
+
+import IMG1 from "./../../assets/photo1.png";
+
+// import { useTranslation } from "react-i18next";
+
 const About = () => {
+  // const { t } = useTranslation();
   return (
-	<div>About</div>
-  )
-}
+    <div>
+      <div className="single-div" style={{ marginTop: 10 }}>
+        <h1
+          className="abouttitlewidth2 marginizer  font-bold leading-tight text-center colorizer"
+          style={{ textTransform: "capitalize" }}
+        >
+          CEO
+        </h1>
+      </div>
+
+      <div className="cardsdep">
+        <div className="carddep">
+          <div className="container3">
+            <div className="carddep">
+              <div className="imgBx">
+                <img src={IMG1} alt="dennis-ritchie" border={0} />
+              </div>
+              <div className="contentBx">
+                <h2>John <br/> Doe</h2>
+                <div className="quote">
+                  <h3>CEO</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carddep">
+          <div className="container3">
+            <div className="carddep">
+              <div className="imgBx">
+                <img src={IMG1} alt="dennis-ritchie" border={0} />
+              </div>
+              <div className="contentBx">
+              <h2>John <br/> Doe</h2>
+                <div className="quote">
+                  <h3>CEO</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="single-div" style={{ marginTop: 10 }}>
+        <h1
+          className="abouttitlewidth2 marginizer  font-bold leading-tight text-center colorizer"
+          style={{ textTransform: "capitalize" }}
+        >
+          CEO
+        </h1>
+      </div>
+
+      <div className="cardsdep">
+        <div className="carddep">
+          <div className="container3">
+            <div className="carddep">
+              <div className="imgBx">
+                <img src={IMG1} alt="dennis-ritchie" border={0} />
+              </div>
+              <div className="contentBx">
+              <h2>John <br/> Doe</h2>
+                <div className="quote">
+                  <h3>CEO</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carddep">
+          <div className="container3">
+            <div className="carddep">
+              <div className="imgBx">
+                <img src={IMG1} alt="dennis-ritchie" border={0} />
+              </div>
+              <div className="contentBx">
+              <h2>John <br/> Doe</h2>
+                <div className="quote">
+                  <h3>CEO</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carddep">
+          <div className="container3">
+            <div className="carddep">
+              <div className="imgBx">
+                <img src={IMG1} alt="dennis-ritchie" border={0} />
+              </div>
+              <div className="contentBx">
+              <h2>John <br/> Doe</h2>
+                <div className="quote">
+                  <h3>CEO</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
 
 export default About
--- src/component/service/Service.js
+++ src/component/service/Service.js
@@ -2,7 +2,55 @@ import React from 'react'
 import './service.css'
 const Service = () => {
   return (
-    <div>Service</div>
+    <>
+    <section class="container1">
+  <div class="section first">
+    <div class="cont_title">
+      <h1>kenai<br/>fjords</h1>
+      <h3>National Park</h3>
+    </div>
+    <div class="cont_desc">
+      <p>Rappel into a crevasse, kayak through icebergs, and watch a glacier recede.</p>
+    </div>
+  </div>
+  <div class="section">
+    <div class="cont_title">
+      <h1>Hawai'i<br/>Volcanoes</h1>
+      <h3>National Park</h3>
+    </div>
+    <div class="cont_desc">
+      <p>Rappel into a crevasse, kayak through icebergs, and watch a glacier recede.</p>
+    </div>
+  </div>
+  <div class="section">
+    <div class="cont_title">
+      <h1>Carlsbad<br/>Caverns</h1>
+      <h3>National Park</h3>
+    </div>
+    <div class="cont_desc">
+      <p>Rappel into a crevasse, kayak through icebergs, and watch a glacier recede.</p>
+    </div>
+  </div>
+  <div class="section">
+    <div class="cont_title">
+      <h1>Bryce<br/>Canyon</h1>
+      <h3>National Park</h3>
+    </div>
+    <div class="cont_desc">
+      <p>Rappel into a crevasse, kayak through icebergs, and watch a glacier recede.</p>
+    </div>
+  </div>
+  <div class="section">
+    <div class="cont_title">
+      <h1>Dry<br/>Tortugas</h1>
+      <h3>National Park</h3>
+    </div>
+    <div class="cont_desc">
+      <p>Rappel into a crevasse, kayak through icebergs, and watch a glacier recede.</p>
+    </div>
+  </div>
+</section>
+    </>
   )
 }
 
